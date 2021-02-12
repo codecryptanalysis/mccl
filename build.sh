b70675bd7bf43114c058a4a90fd0baa6b0aebb26
cr-marcstevens <github@marc-stevens.nl>
2021-02-12 16:22:03 +0100

    convenience: build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,11 @@ else
 fi
 }
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+if [ ! -f mccl/config/config.h ] || [ "x$1" != "x" ]; then
 docmd autoreconf --install
 docmd ./configure
-docmd make clean
+fi
 docmd make all
 docmd make check
