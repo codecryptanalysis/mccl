53800953269be686f314a1b9c356beb5d1c312df
Wessel <wesselvanwoerden@gmail.com>
2021-05-10 13:58:36 +0200

    ISD_single_generic for ell=0, untested

--- mccl/algorithm/decoding.hpp
+++ mccl/algorithm/decoding.hpp
@@ -161,6 +161,19 @@ public:
 };
 
 
+template<typename data_t>
+bool check_solution(const mccl::matrix_ref_t<data_t> &H01T_view, const vector_ref_t<data_t>& S0, const std::vector<uint32_t>& perm, size_t w, const std::vector<uint32_t>& E1_sparse, size_t w1) {
+    vector_ref_t<data_t> tmp = S0;
+    for( auto i : E1_sparse ) {
+        tmp += H01T_view[i];
+    }
+    if(hammingweight(tmp)< w-w1-E1_sparse.size()) {
+        // submit solution?
+        return true;
+    }
+    return false;
+}
+
 /*
 This is the generic algorithm solving target-ISD using a to-be-specified subISD algorithm
 Therefore it is derived from ISD_API_target<data_t>.
@@ -174,6 +187,18 @@ In that case, be sure to make the crucial functions as final.
 template<typename data_t, typename subISD_t = ISD_API_exhaustive<data_t> >
 class ISD_single_generic: public ISD_API_single<data_t>
 {
+private:
+    mccl::matrix_t<data_t>* H_ptr = nullptr;
+    mccl::matrix_t<data_t>* H01T = nullptr;
+    mccl::vector_t<data_t>* S_ptr = nullptr;
+    mccl::matrix_ref_t<data_t>* H01_S_view = nullptr;
+    mccl::matrix_ref_t<data_t>* H01T_view = nullptr;
+    mccl::matrix_ref_t<data_t>* H01T_S_view = nullptr;
+    mccl::matrix_ref_t<data_t>* H11_S_view = nullptr;
+    mccl::matrix_ref_t<data_t>* H11T_view = nullptr;
+    mccl::matrix_ref_t<data_t>* H11T_S_view = nullptr;
+    matrix_permute_t<uint64_t>* permutator = nullptr;
+    size_t n,k,w,rows,cols0,cols1,scratch0,cnt;
 public:
     ISD_single_generic(subISD_t& sI)
         : subISD(&sI)
@@ -184,11 +209,35 @@ public:
     //virtual void configure(const parameters_t& params) = 0;
     
     // deterministic initialization for given parity check matrix H0 and target syndrome s0
-    void initialize(const matrix_ref_t<data_t>& _H0, const vector_ref_t<data_t>& _s0, unsigned int _w) final
-    {
-        H0 = _H0;
-        s0 = _s0;
-        w = _w;
+    void initialize(const matrix_ref_t<data_t>& H_, const vector_ref_t<data_t>& S, unsigned int w_) {
+        cnt = 0;
+        w = w_;
+        n = H_.columns();
+        k = n-H_.rows();
+        rows = n-k;
+        cols0 = n-k;
+        scratch0 = get_scratch(cols0, 64);
+        cols1 = k;
+        H_ptr = new mccl::matrix_t<data_t>(n-k, cols0+scratch0+cols1+1); // +1 to store S
+        for( size_t i = 0; i < rows; i++ ) {
+            for( size_t j = 0; j < cols0; j++ ) {
+                H_ptr->bitset(i,j+scratch0,H_(i,j));
+            }
+            for( size_t j = 0; j < cols1; j++ ) {
+                H_ptr->bitset(i, cols0+scratch0+j, H_(i, cols0+j));
+            }
+            H_ptr->bitset(i, cols0+scratch0+cols1, S[i]);
+        }
+
+        size_t scratch1 = get_scratch(cols1+1, 64);
+        H01_S_view = new mccl::matrix_ref_t<data_t>(H_ptr->submatrix(0, rows, cols0+scratch0, cols1+1, scratch1));
+
+        H01T = new mccl::matrix_t<data_t>(cols1+1, rows);
+        size_t scratch01T = get_scratch(rows, 64);
+        H01T_S_view = new mccl::matrix_ref_t<data_t>(H01T->submatrix(0, cols1+1, 0, rows, scratch01T));
+        H01T_view = new mccl::matrix_ref_t<data_t>(H01T->submatrix(0, cols1, 0, rows, scratch01T));
+
+        permutator = new matrix_permute_t<uint64_t>(*H_ptr);
     }
     
     // probabilistic preparation of loop invariant
@@ -197,8 +246,34 @@ public:
     }
     
     // perform one loop iteration, return true if successful and store result in e
-    bool loop_next() final
-    {
+    bool loop_next() {
+        cnt++;
+        permutator->random_permute(scratch0, scratch0+cols0, scratch0+n);
+        auto pivotend = echelonize(*H_ptr, scratch0, scratch0+cols0);
+        if(pivotend != cols0)
+            return true;
+
+        H01T_S_view->transpose(*H01_S_view);
+        auto S0 = (*H01T_S_view)[cols1];
+        auto perm = permutator->get_permutation();
+        bool done = false;
+        auto callback = [this, &perm, &S0, &done](const std::vector<uint32_t>& E1_sparse, size_t w1){
+            if(check_solution(*(this->H01T_view), S0, perm, this->w, E1_sparse, w1))
+                done = true;
+        };
+
+        // still assuming ell=0
+        vector_ref_t<data_t> S1; // todo: take from H11T_S_view if ell>0
+        size_t w1_max = 0;
+        subISD.initialize(H11T_view, S1, w1_max, callback);
+        subISD.solve();
+        subISD.free();
+
+        if(done) {
+            std::cerr << "Found solution after " << cnt << " iterations." << std::endl;
+            return false;
+        }
+        return true;
     }
     
     // run loop until a solution is found
@@ -210,11 +285,6 @@ public:
     }
     
     subISD_t* subISD;
-    
-    vector_t<data_t> e;
-    matrix_t<data_t> H0, H;
-    vector_t<data_t> s0, s;
-    unsigned int w;
 };
 
 MCCL_END_NAMESPACE
