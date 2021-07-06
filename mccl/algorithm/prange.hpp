4c586ec5d9f5a575ec7822b355d15f39fa6c5085
cr-marcstevens <github@marc-stevens.nl>
2021-07-06 17:09:49 +0200

    first subISDT: subISDT_prange

--- mccl/algorithm/prange.hpp
+++ mccl/algorithm/prange.hpp
@@ -28,6 +28,42 @@ private:
     std::vector<uint32_t> E1_sparse;
 };
 
+
+class subISDT_prange
+    : public ISD_API_exhaustive_transposed_sparserange_t
+{
+public:
+    using ISD_API_exhaustive_transposed_sparserange_t::callback_t;
+    
+    void initialize(const cmat_view& HTpadded, size_t HTcolumns, const cvec_view& Spadded, unsigned int w, callback_t _callback, void* _ptr) final
+    {
+        // should only be used with l=0
+        if (HTcolumns != 0)
+            throw;
+        callback = _callback;
+        ptr = _ptr;
+    }
+    
+    void prepare_loop() final
+    {
+    }
+    
+    bool loop_next() final
+    {
+        (*callback)(ptr, nullptr, nullptr, 0);
+        return false;
+    }
+    
+    void solve() final
+    {
+        loop_next();
+    }
+    
+private:
+    callback_t callback;
+    void* ptr;
+};
+
 MCCL_END_NAMESPACE
 
 #endif
